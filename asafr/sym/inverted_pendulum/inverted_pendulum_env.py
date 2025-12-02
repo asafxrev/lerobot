9bb864f1d3225c6ca65f14e88d1b6f5f8a1e46ee
asafxrev <asafxrev@gmail.com>
2025-12-01 19:26:58 -0500

    Adding RL (REINFORCE) colab and a no-UI env variant

--- asafr/sym/inverted_pendulum/inverted_pendulum_env.py
+++ asafr/sym/inverted_pendulum/inverted_pendulum_env.py
@@ -3,6 +3,11 @@
 import gymnasium as gym
 
 def make_env(n_envs: int = 1, use_async_envs: bool = False):
+    return make_env_detailed(n_envs, use_async_envs, render_mode='human')
+
+
+def make_env_detailed(n_envs: int = 1, use_async_envs: bool = False, 
+                      render_mode: str = 'human'):
     """
     Create vectorized environments for your custom task.
 
@@ -16,7 +21,7 @@ def make_env(n_envs: int = 1, use_async_envs: bool = False):
     def _make_single_env():
         # Create your custom environment
         return gym.make('InvertedPendulum-v5', reset_noise_scale=0.1, 
-                        render_mode='human',
+                        render_mode=render_mode,
                         width=1280, height=720)
 
     # Choose vector environment type
---
+++ asafr/sym/inverted_pendulum/inverted_pendulum_env_no_ui.py
@@ -0,0 +1,18 @@
+# See https://gymnasium.farama.org/environments/mujoco/inverted_pendulum/
+
+import gymnasium as gym
+import inverted_pendulum_env
+
+def make_env(n_envs: int = 1, use_async_envs: bool = False):
+    """
+    Create vectorized environments for your custom task.
+
+    Args:
+        n_envs: Number of parallel environments
+        use_async_envs: Whether to use AsyncVectorEnv or SyncVectorEnv
+
+    Returns:
+        gym.vector.VectorEnv or dict mapping suite names to vectorized envs
+    """
+    return inverted_pendulum_env.make_env_detailed(
+        n_envs, use_async_envs, render_mode=None)
